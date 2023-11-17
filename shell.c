025630a7e2c80023342762b7dfba21b5920237ad
orjimiracle1 <orjimiraclealx@gmail.com>
2023-11-16 16:36:53 -0800

    correction

--- shell.c
+++ shell.c
@@ -15,18 +15,18 @@
 int main(int argc __attribute__((unused)), char **argv, char **env)
 {
 	char display_prompt[2] = "$ ", *buffer = NULL, *trim_buffer = NULL;
-	int buffer_size = 2, getline_rvalue = 0, True = 1, status;
+	int getline_rvalue = 0, True = 1, status;
 	pid_t p_id;
+	size_t buffer_size = 0;
 
 	while (True)
 	{
 		if (isatty(STDIN_FILENO))
 			print_display(display_prompt);
-		buffer = malloc(sizeof(char) * buffer_size);
-		getline_rvalue = my_getline(&buffer, &buffer_size, stdin);
-		trim_buffer = my_strtrim(buffer);
+		getline_rvalue = getline(&buffer, &buffer_size, stdin);
 		if (getline_rvalue == -1)
-			free(buffer), exit(0);
+			exit(0);
+		trim_buffer = my_strtrim(buffer);
 		if (trim_buffer != NULL && *trim_buffer == '\n')
 		{
 			free(buffer);
